Meinersbur/pet

commit 01193ea831b938dbecf43bdea7eeac8ccfa275b6

accept compound statements with zero statements when autodetecting
Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

scan.cc
@@ -2173,10 +2173,7 @@ struct pet_scop *PetScan::extract(StmtRange stmt_range)
 	int j;
 	bool partial_range = false;
 
-	if (autodetect)
-		scop = NULL;
-	else
-		scop = pet_scop_empty(ctx);
+	scop = pet_scop_empty(ctx);
 	for (i = stmt_range.first, j = 0; i != stmt_range.second; ++i, ++j) {
 		Stmt *child = *i;
 		struct pet_scop *scop_i;
@@ -2187,13 +2184,11 @@ struct pet_scop *PetScan::extract(StmtRange stmt_range)
 		}
 		scop_i = pet_scop_prefix(scop_i, j);
 		if (autodetect) {
-			if (!scop) {
-				if (j != 0)
-					partial_range = true;
-				scop = scop_i;
-			} else if (scop_i)
+			if (scop_i)
 				scop = pet_scop_add(ctx, scop, scop_i);
 			else
+				partial_range = true;
+			if (scop->n_stmt != 0 && !scop_i)
 				partial = true;
 		} else {
 			scop = pet_scop_add(ctx, scop, scop_i);

commit 61fc3b8f78c581e4091a767b84ee57ab4670921c

fix handling of compound statements with initial partial scop when autodetecting
If the partial scop is the first scop we find in the compound statement,
then we intended to keep it as the scop of the compound statement,
but this behavior was broken in 01193ea (accept compound statements with
zero statements when autodetecting, Mon Aug 22 13:39:33 2011 +0200).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

scan.cc
@@ -4875,7 +4875,7 @@ struct pet_scop *PetScan::extract(StmtRange stmt_range, bool block,
 			continue;
 
 		scop_i = extract(child);
-		if (scop && partial) {
+		if (scop->n_stmt != 0 && partial) {
 			pet_scop_free(scop_i);
 			break;
 		}