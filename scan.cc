Meinersbur/pet

commit 61fc3b8f78c581e4091a767b84ee57ab4670921c

fix handling of compound statements with initial partial scop when autodetecting
If the partial scop is the first scop we find in the compound statement,
then we intended to keep it as the scop of the compound statement,
but this behavior was broken in 01193ea (accept compound statements with
zero statements when autodetecting, Mon Aug 22 13:39:33 2011 +0200).

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

scan.cc
@@ -4875,7 +4875,7 @@ struct pet_scop *PetScan::extract(StmtRange stmt_range, bool block,
 			continue;
 
 		scop_i = extract(child);
-		if (scop && partial) {
+		if (scop->n_stmt != 0 && partial) {
 			pet_scop_free(scop_i);
 			break;
 		}

commit f73aaba0d817f6c2fe50295b1e027951e77ae233

PetScan::extract(StmtRange, bool, bool): break out early on NULL scop
Commit 61fc3b8 (fix handling of compound statements with initial partial
scop when autodetecting, Tue Jul 30 11:19:56 2013 +0200) introduced
a test on one of the members of scop, which breaks down if scop has
become NULL due to some (parsing) error.

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

scan.cc
@@ -4909,7 +4909,7 @@ struct pet_scop *PetScan::extract(StmtRange stmt_range, bool block,
 
 		scop = skip.add(scop, j);
 
-		if (partial)
+		if (partial || !scop)
 			break;
 	}
 