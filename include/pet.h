c7434b7377cc13a9db71288b44d6f6ddc24351a4
Sven Verdoolaege <skimo@kotnet.org>
2015-01-23 23:25:04 +0100

    add pet_tree_map_call_expr
    This allows users to modify all calls in a pet_tree.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/pet.h
+++ include/pet.h
@@ -339,6 +339,10 @@ int pet_tree_foreach_expr(__isl_keep pet_tree *tree,
 /* Call "fn" on each access subexpression in the nodes of "tree" */
 int pet_tree_foreach_access_expr(__isl_keep pet_tree *tree,
 	int (*fn)(__isl_keep pet_expr *expr, void *user), void *user);
+/* Modify all call subexpressions in the nodes of "tree" through "fn". */
+__isl_give pet_tree *pet_tree_map_call_expr(__isl_take pet_tree *tree,
+	__isl_give pet_expr *(*fn)(__isl_take pet_expr *expr, void *user),
+	void *user);
 
 void pet_tree_dump(__isl_keep pet_tree *tree);
 
