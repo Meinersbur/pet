6486c770f936b276be1c7742693b45abcd1bee8c
Michael Kruse <pet@meinersbur.de>
2016-02-05 20:47:40 +0100

    Ignore all warnings

--- pet.cc
+++ pet.cc
@@ -1,19 +1,19 @@
 /*
  * Copyright 2011      Leiden University. All rights reserved.
  * Copyright 2012-2014 Ecole Normale Superieure. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY LEIDEN UNIVERSITY ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,12 +25,12 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation
  * are those of the authors and should not be interpreted as
  * representing official policies, either expressed or implied, of
  * Leiden University.
- */ 
+ */
 
 #include "config.h"
 
@@ -1005,6 +1005,7 @@ static int foreach_scop_in_C_source(isl_ctx *ctx,
 	CompilerInstance *Clang = new CompilerInstance();
 	create_diagnostics(Clang);
 	DiagnosticsEngine &Diags = Clang->getDiagnostics();
+	Diags.setIgnoreAllWarnings(true); // Switch off all warnings as they are not useful; We were preprocessing the source file using another compiler (usually gcc), which can insert compiler-specific things such as attributes.
 	Diags.setSuppressSystemWarnings(true);
 	CompilerInvocation *invocation = construct_invocation(filename, Diags);
 	if (invocation)
